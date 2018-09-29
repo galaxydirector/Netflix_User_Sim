4312b4d48bfb8a1dd76dac2140e517851fdd48b6
RenzhiHuang <renzhihuang0106@gmail.com>
2018-09-28 23:19:37 -0400

    fix the 5th

--- get_queried_user.py
+++ get_queried_user.py
@@ -1,11 +1,11 @@
-def get_queried_user(pairs, queried_user_id):
+def get_queried_user(user_dic, sorted_username, queried_user_id):
 	user_index = sorted_username.index(queried_user_id)
-	ret = []
-	for pair in pairs:
-		if user_index in pair:
-			if pair[0]==user_index:
-				partner = pair[1]
-			else:
-				partner = pair[0]
-			ret.append(partner)
-	return ret
+	min_dis = 1
+	curr_nearest = -1
+	for i in range(len(user_dic)):
+		if(i != user_index):
+			curr_dis = 1-(len(set(user_dic[user_index]).intersection(set(user_dic[i]))))/len(set(user_dic[user_index]+user_dic[i]))
+			if(curr_dis<min_dis):
+				min_dis = curr_dis
+				curr_nearest = i
+	return sorted_username[i]
